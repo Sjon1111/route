5ed81d29253082891a6a06a48a4ffd44819164e2
Sjon1111 <145475256+Sjon1111@users.noreply.github.com>
2023-10-16 01:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
+
 const port = 8000; // You can choose any available port
 
 // Middleware to parse JSON in the request body
